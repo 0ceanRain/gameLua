fa60a3ba5581a75e6a6ba2b7d90f3ee6b47cb032
qcookie-1 <cookieeatpoo247@gmail.com>
2025-07-17 01:08:41 +1000

    new file

---
+++ Shared/Teleport.lua
@@ -0,0 +1,23 @@
+local TeleportModule = {}
+
+
+local CombatStateModule = require(game:GetService("RobloxReplicatedStorage").Modules.CombatStateModule)
+
+local CanActivate = true
+
+local function CanActivate(player)
+    if CombatStateModule.IsStunned(player) or CombatStateModule.IsDoingAction(player) or not CanActivate then 
+		return false 
+	elseif CanActivate then
+		return true
+	end
+end
+
+
+
+
+local function Activate()
+    CanActivate = false
+end
+
+return TeleportModule
