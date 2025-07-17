12f1358ebe55d6d9680feb7965fea8d899185977
0ceanRain <0ceanrain9.2@gmail.com>
2025-07-17 21:42:25 +1000

    Update DashServer.lua

--- ServerScripts/DashServer.lua
+++ ServerScripts/DashServer.lua
@@ -1,8 +1,15 @@
-local dashRemote = game.ReplicatedStorage.Remotes.DashRemote
+local dashRemote = game.ReplicatedStorage.Remotes.DefenseRemotes.DashRemote
 
-dashRemote.OnServerEvent:Connect(function(player, amount)
+dashRemote.OnServerEvent:Connect(function(player, amount, string)
 	local character = player.Character
 	if amount == 1 then
+		if string == "invul" then
+			character:SetAttribute("IsDashing", true)
+			print("invul")
+		else
+			character:SetAttribute("IsDashing", false)
+		end 
+
 		dashRemote:FireClient(player)
 		for i,v in pairs(character:GetDescendants()) do 
 			if v:IsA("BasePart") or v:IsA("Decal") and not v:HasTag("VFX") then 
@@ -12,6 +19,7 @@ dashRemote.OnServerEvent:Connect(function(player, amount)
 		end
 	end	
 	if amount == 2 then
+		character:SetAttribute("IsDashing", false)
 		for i,v in pairs(character:GetDescendants()) do 
 			if v:IsA("BasePart") or v:IsA("Decal") and not v:HasTag("VFX") then 
 				v.Transparency = 0
