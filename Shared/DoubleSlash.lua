ec07dad4664b3f3cc4d1725f40f236866ad4ddbc
qcookie-1 <cookieeatpoo247@gmail.com>
2025-07-20 00:42:16 +1000

    done

--- Shared/DoubleSlash.lua
+++ Shared/DoubleSlash.lua
@@ -1,23 +1,53 @@
-local TeleportModule = {}
-
+local DoubleSlashModule = {
+	
+}
+DoubleSlashModule.cooldown = 8
+DoubleSlashModule.windup1 = 0.5
+DoubleSlashModule.windup2 = 0.8
 
 local CombatStateModule = require(game:GetService("RobloxReplicatedStorage").Modules.CombatStateModule)
 
 local CanActivate = true
 
-local function CanActivate(player)
+local attackRemote = game.ReplicatedStorage.Remotes.attackRemote
+
+function DoubleSlashModule:CanActivate(player)
     if CombatStateModule.IsStunned(player) or CombatStateModule.IsDoingAction(player) or not CanActivate then 
 		return false 
 	elseif CanActivate then
 		return true
 	end
 end
-
+local Swing1Finshed = false
+local Swing2Finshed = false
 
 -- ok
+function DoubleSlashModule:Activate(player)
+	local swing1Anim = game.ReplicatedStorage.animations.abilities.DoubleSlash1
+	local swing2Anim = game.ReplicatedStorage.animations.abilities.DoubleSlash2
+	CanActivate = true
 
-local function Activate()
-    CanActivate = false
+	swing1Anim:Play()
+
+	local FirstSwing = task.delay(self.windup1, function()
+		
+		attackRemote:FireServer(player)
+
+		Swing1Finshed = true
+	end)
+
+	if Swing1Finshed then
+		Swing1Finshed = false
+		swing2Anim:Play()
+		task.delay(self.windup2, function()
+			attackRemote:FireServer(player)
+
+		end)
+	end
+	task.delay(self.cooldown, function()
+		CanActivate = true
+	end)
+    
 end
 
-return TeleportModule
+return DoubleSlashModule
